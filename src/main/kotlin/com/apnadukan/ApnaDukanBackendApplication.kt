d7dea11f52a1baef67dc2248de49521053d9bace
Biswajeet <biswajeet162@gmail.com>
2026-01-03 19:54:43 +0530

    init

---
+++ src/main/kotlin/com/apnadukan/ApnaDukanBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.apnadukan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApnaDukanBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApnaDukanBackendApplication>(*args)
+}
